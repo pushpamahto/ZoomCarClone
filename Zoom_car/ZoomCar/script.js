1a665c3181bddb4d4d515ee0689ef45c982c0832
pushpamahto <pushpamahto764@gmail.com>
2023-11-09 23:56:17 +0530

    Zoomcar clone Assignment

---
+++ Zoom_car/ZoomCar/script.js
@@ -0,0 +1,29 @@
+// Navbar Dropdown menu
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+// Side NAv BAr
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
